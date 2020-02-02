83b0050c88214481e8467fcffc9a48f6e57748e1
Theo Buchholz <theobuchholz@gmail.com>
2020-02-02 11:31:56 -0600

    fixed drive motor ids

--- Infinite Recharge/src/main/include/blitzLib/include/DriveReferences.hpp
+++ Infinite Recharge/src/main/include/blitzLib/include/DriveReferences.hpp
@@ -5,15 +5,15 @@ namespace Blitz
     class DriveReference
     {
         public:
-            static constexpr int LEFT_FRONT_MOTOR_ID = 4;
-            static constexpr int LEFT_BACK_MOTOR_ID = 3;
-            static constexpr int RIGHT_FRONT_MOTOR_ID = 1;
-            static constexpr int RIGHT_BACK_MOTOR_ID = 2;
+            static constexpr int LEFT_FRONT_MOTOR_ID = 1;
+            static constexpr int LEFT_BACK_MOTOR_ID = 2;
+            static constexpr int RIGHT_FRONT_MOTOR_ID = 3;
+            static constexpr int RIGHT_BACK_MOTOR_ID = 4;
 
             static constexpr double RAMP_RATE = .4;
 
             static constexpr double ENCODER_UNITS_PER_ROTATION = 42;
-            static constexpr double MAX_RPM = 5600;
+            static constexpr double MAX_RPM = 5500;
 
             static constexpr double MAX_OUPUT = 1;
     };
